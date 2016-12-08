0eb9395438b25b1797e3009d392f9a18ba30c094
Charles Twardy <ctwardy@gmail.com>
2016-12-08 15:25:08 -0500

    Added SciKit Classifier.ipynb: implements "ML Pipelines 2" in SciKit-Learn. * Omits URL features per #24.

--- tools/src/main/python/time_v_pop.py
+++ tools/src/main/python/time_v_pop.py
@@ -1,13 +1,12 @@
-import sys, re
-import csv
-from utils import *
 from collections import defaultdict
 
-tsv_file = csv.reader(CommentedFile(open(   sys.argv[1], "rb")), delimiter='\t')
+from utils import *
+
+tsv_file = csv.reader(CommentedFile(open(sys.argv[1], "rb")), delimiter='\t')
 
 ym = 17
 floor = 0
-floorfrac = 0.1/100.0
+floorfrac = 0.1 / 100.0
 start = 0
 
 nf = defaultdict(int)
@@ -17,13 +16,13 @@ for row in tsv_file:
     if start == 0:
         start = 1
         continue
-    #print row
+    # print row
     ys = 0
     yt = 0
     tot = 0
     fy = 0
     ly = 0
-    for yd in range(1,ym+1):
+    for yd in range(1, ym + 1):
         tot += int(row[yd])
         if int(row[yd]) > floor:
             if ys == 0:
@@ -41,12 +40,12 @@ for row in tsv_file:
     # Collect last year found:
     nl[ly] += 1
     # Output timespans
-    #if int(row[ym]) <= floor and int(row[ym-1]) <= floor:
-    #if int(row[1]) <= floor and int(row[2]) <= floor and int(row[3]) <= floor:
+    # if int(row[ym]) <= floor and int(row[ym-1]) <= floor:
+    # if int(row[1]) <= floor and int(row[2]) <= floor and int(row[3]) <= floor:
     if yt > 0:
-        if float(row[ym])/tot <= floorfrac and int(row[ym-1])/tot <= floorfrac:
-            print "{}\t{}\t{}".format(row[0],yt,tot)
+        if float(row[ym]) / tot <= floorfrac and int(row[ym - 1]) / tot <= floorfrac:
+            print "{}\t{}\t{}".format(row[0], yt, tot)
 
-#print "Year\tFirst Observed\tLast Observed"
-#for y in range(1,ym+1):
+# print "Year\tFirst Observed\tLast Observed"
+# for y in range(1,ym+1):
 #    print "{}\t{}\t{}".format( y+1993, nf[y], nl[y] )
